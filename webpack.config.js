43e9c6c456720ee0d5178d3c2c738338350b6ed1
Sunny Golovine <sunny@glvn.co>
2021-03-10 15:53:34 -0500

    Remove cache from terser plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,6 @@ module.exports = {
     minimizer: [
       new TerserPlugin({
         parallel: true,
-        cache: true,
         extractComments: "all",
         terserOptions: {
           warnings: true,
