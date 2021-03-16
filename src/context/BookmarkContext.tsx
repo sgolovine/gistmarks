37e49f8fd4fb54b4db4a5783db2949baf3af9750
Sunny Golovine <sunny@glvn.co>
2021-03-16 02:07:49 -0400

    Add active collections, launch tasks

--- src/context/BookmarkContext.tsx
+++ src/context/BookmarkContext.tsx
@@ -1,6 +1,6 @@
-import React, { createContext } from "react"
+import React, { createContext, useEffect, useState } from "react"
 import { BOOKMARK_STORAGE_KEY } from "~/defines"
-import { omitKey } from "~/helpers"
+import { omitKey, removeItem, uniq } from "~/helpers"
 import useLocalStorage from "~/hooks/useLocalStorage"
 import { Bookmark } from "~/model/Bookmark"
 import { ContextProviderProps } from "~/model/Context"
@@ -11,6 +11,10 @@ type Bookmarks = {
 
 interface BookmarkContext {
   bookmarks: Bookmarks
+  categories: string[]
+  activeCategories: string[]
+  addCategory: (category: string) => void
+  removeCategory: (category: string) => void
   addBookmark: (bookmark: Bookmark, guid: string) => void
   removeBookmark: (guid: string) => void
   editBookmark: (bookmark: Partial<Bookmark>, guid: string) => void
@@ -18,6 +22,10 @@ interface BookmarkContext {
 
 export const BookmarkContext = createContext<BookmarkContext>({
   bookmarks: {},
+  categories: [],
+  activeCategories: [],
+  addCategory: () => null,
+  removeCategory: () => null,
   addBookmark: () => null,
   removeBookmark: () => null,
   editBookmark: () => null,
@@ -26,39 +34,87 @@ export const BookmarkContext = createContext<BookmarkContext>({
 export const BookmarkContextProvider: React.FC<ContextProviderProps> = ({
   children,
 }) => {
-  const [_bookmarks, _setBookmarks] = useLocalStorage<Bookmarks>(
+  const [bookmarks, setBookmarks] = useLocalStorage<Bookmarks>(
     BOOKMARK_STORAGE_KEY,
     {}
   )
+  const [filteredBookmarks, setFilteredBookmarks] = useState<Bookmarks>(
+    bookmarks
+  )
+
+  const [categories, setCategories] = useState<string[]>([])
+
+  const [activeCategories, setActiveCategories] = useState<string[]>([])
+
+  useEffect(() => {
+    const bookmarkKeys = Object.keys(bookmarks)
+    const categories = bookmarkKeys.reduce((acc: string[], key: string) => {
+      return [...acc, bookmarks[key].category]
+    }, [])
+    setCategories(categories)
+  }, [bookmarks])
+
+  useEffect(() => {
+    if (activeCategories.length > 0) {
+      const filterBookmarks = Object.keys(bookmarks).reduce(
+        (acc: Bookmarks, key: string) => {
+          if (activeCategories.indexOf(bookmarks[key].category) > -1) {
+            return {
+              ...acc,
+              [key]: bookmarks[key],
+            }
+          } else {
+            return acc
+          }
+        },
+        {} as Bookmarks
+      )
+      setFilteredBookmarks(filterBookmarks)
+    } else {
+      setFilteredBookmarks(bookmarks)
+    }
+  }, [bookmarks, categories])
 
   const addBookmark = (bookmark: Bookmark, guid: string) => {
-    _setBookmarks({
-      ..._bookmarks,
+    setBookmarks({
+      ...bookmarks,
       [guid]: bookmark,
     })
   }
   const removeBookmark = (guid: string) => {
-    const newState = omitKey(guid, _bookmarks)
-    _setBookmarks(newState)
+    const newState = omitKey(guid, bookmarks)
+    setBookmarks(newState)
   }
 
   const editBookmark = (bookmark: Partial<Bookmark>, guid: string) => {
-    if (_bookmarks[guid]) {
-      _setBookmarks({
-        ..._bookmarks,
+    if (bookmarks[guid]) {
+      setBookmarks({
+        ...bookmarks,
         [guid]: {
-          ..._bookmarks[guid],
+          ...bookmarks[guid],
           ...bookmark,
         },
       })
     }
   }
 
+  const addCategory = (category: string) => {
+    setActiveCategories(uniq([...activeCategories, category]))
+  }
+
+  const removeCategory = (category: string) => {
+    setActiveCategories(removeItem(activeCategories, category))
+  }
+
   const value: BookmarkContext = {
-    bookmarks: _bookmarks,
+    bookmarks: filteredBookmarks,
+    activeCategories,
+    categories,
     addBookmark,
     editBookmark,
     removeBookmark,
+    addCategory,
+    removeCategory,
   }
 
   return (
