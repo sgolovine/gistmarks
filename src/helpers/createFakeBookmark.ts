5695ee7c99872c9954cc3619b99bf4995b76aaf8
Sunny Golovine <sunny@glvn.co>
2021-04-14 08:42:11 -0400

    Remove faker

--- src/helpers/createFakeBookmark.ts
+++ src/helpers/createFakeBookmark.ts
@@ -1,14 +1,13 @@
-import faker from "faker"
 import { Bookmark } from "~/model/Bookmark"
 import { generateUUID } from "./generateGuid"
 
 export function createFakeBookmark(): Bookmark {
   const bookmark: Bookmark = {
     guid: generateUUID(),
-    name: faker.lorem.words(),
-    href: faker.internet.url(),
-    category: faker.lorem.word(),
-    description: faker.lorem.sentence(),
+    name: "Bookmark Name",
+    href: "https://www.gistmarks.io",
+    category: "Test Category",
+    description: "This is a test description",
   }
   return bookmark
 }
